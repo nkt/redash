5465cb1925f21a452c035fc0c5e37295c33ec760
Nikita Gusakov <gusnkt@gmail.com>
2021-11-29 14:53:10 +0300

    feat: force set en-gb locale

--- client/app/index.js
+++ client/app/index.js
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom";
 
 import "@/config";
 
+import ConfigProvider from "antd/lib/config-provider";
+import en_GB from "antd/lib/locale-provider/en_GB";
+import moment from "moment";
+import "moment/locale/en-gb";
+
 import ApplicationArea from "@/components/ApplicationArea";
 import offlineListener from "@/services/offline-listener";
 
-ReactDOM.render(<ApplicationArea />, document.getElementById("application-root"), () => {
-  offlineListener.init();
-});
+moment.locale("en-gb");
+
+ReactDOM.render(
+  <ConfigProvider locale={en_GB}>
+    <ApplicationArea />
+  </ConfigProvider>,
+  document.getElementById("application-root"),
+  () => {
+    offlineListener.init();
+  }
+);
